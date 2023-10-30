1daeb597a5efe802b723588d4ba6ec27c1170545
Chimelumo <chimelum.okonkwo@cttech.org>
2023-10-30 18:19:07 -0400

    Uploading my python code

---
+++ main.py
@@ -0,0 +1,43 @@
+def main():
+  try:
+      userName = input("Chimelumo ")
+      userName = userName
+  except Exception as ourException:
+      print( "Ooops an error occured", ourException )
+  else:
+      print( userName )
+
+  playAgain = True
+
+  while playAgain == True:
+    myFavoriteCrops = ["apple", "corn", "cherry"]
+    print(myFavoriteCrops)
+    print("Welcome to the app")
+    thislist = ["day 1", "day 2", "day 3"]
+    print(len(thislist))
+    crop = "apple"
+    print(f"The chosen crop is {crop}")
+    crop = "corn"
+    print(f"Now the chosen crop is {crop}")
+    crop1 = input("What crop do you like: ")
+    if crop1 == "apple":
+      print("I like apples")
+    elif crop1 == "cherry":
+      print("I like cherries")
+    elif crop1 == "corn":
+      print("I like corn") 
+    season = ["spring", "summer", "fall", "winter"]
+    if season == "spring":
+      print("Woohooo!!!! We have enough crops")
+    elif season == "winter":
+      print("Oh no we don't have enough food")
+
+    playAgain = input("Do you want to play again. Enter y or n: ")
+    if playAgain == 'n':
+      print("Thanks for playing")
+      
+    elif playAgain == 'y':
+      print("Let's play again")
+      main()
+
+main()
